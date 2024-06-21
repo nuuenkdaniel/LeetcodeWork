8ca291aac4ab27d184eedb494182f4ea2948df95
Daniel <lewdaniel2@gmail.com>
2024-06-21 10:48:23 -0400

    finished add two numbers problem

--- Add_Two_Numbers.java
+++ Add_Two_Numbers.java
@@ -1,5 +1,5 @@
 public class Add_Two_Numbers {
-  
+
   //While loop through both linked list and add the values together while carrying over when necessary
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     int carryOver = (l1.val + l2.val)/10;
@@ -8,24 +8,18 @@ public class Add_Two_Numbers {
     while(l1.next != null && l2.next != null) {
       l1 = l1.next;
       l2 = l2.next;
-      carryOver = (l1.val + l2.val)/10;
-      l3.next = new ListNode((l1.val + l2.val)%10);
+      l3.next = new ListNode((l1.val + l2.val + carryOver)%10);
+      carryOver = (l1.val + l2.val + carryOver)/10;
       l3 = l3.next;
     }
     //Once the end of one of the linked list append to the end of the new linked list and carrying over when necessary
-    ListNode trailingL = l1.next == null? l2 : l1;
+    l3.next = l1.next == null? l2.next : l1.next;
     while(carryOver != 0) {
-      if(trailingL.next == null) {
-        trailingL.next = new ListNode(1);
-        break;
-      }
-      else {
-        trailingL = trailingL.next;
-        carryOver = (trailingL.val+1)/10;
-        trailingL.val = (trailingL.val+1)%10;
-      }
+      if(l3.next == null && carryOver != 0) l3.next = new ListNode(0);
+      l3 = l3.next;
+      carryOver = (l3.val + 1)/10;
+      l3.val = (l3.val + 1)%10;
     }
     return head;
-
   }
 }
