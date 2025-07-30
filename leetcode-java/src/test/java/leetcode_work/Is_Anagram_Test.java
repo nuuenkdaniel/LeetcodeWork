c3cbc9948d194c56e3c39776dafab6fa16356824
Daniel <lewdaniel2@gmail.com>
2025-07-30 17:36:05 -0400

    Test different length and non anagrams

--- leetcode-java/src/test/java/leetcode_work/Is_Anagram_Test.java
+++ leetcode-java/src/test/java/leetcode_work/Is_Anagram_Test.java
@@ -14,9 +14,17 @@ class Is_Anagram_Test {
 
   @Test
   void test_different_length() {
+    String s1 = "poop";
+    String s2 = "poopy";
+    Boolean result = Is_Anagram.isAnagram(s1, s2);
+    assertEquals(false, result);
   }
 
   @Test
   void test_NonAnagram() {
+    String s1 = "JohnWick";
+    String s2 = "Jhimothy";
+    Boolean result = Is_Anagram.isAnagram(s1, s2);
+    assertEquals(true, result);
   }
 }
