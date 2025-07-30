a5018d81fdb8aebb446a42bb7603efb2d7ff0d0a
Daniel <lewdaniel2@gmail.com>
2025-07-30 17:33:01 -0400

    defined curr_char_count as an Integer type

--- leetcode-java/src/main/java/leetcode_work/Is_Anagram.java
+++ leetcode-java/src/main/java/leetcode_work/Is_Anagram.java
@@ -11,14 +11,14 @@ public class Is_Anagram {
       for(int i = 0; i < s.length(); i++) {
         // Input string s
         char curr_char = s.charAt(i);
-        int curr_char_count = char_count_s.get(curr_char);
-        if(curr_char_count != null && curr_char_count != 0) char_count_s.put(curr_char, curr_char_count+1);
+        Integer curr_char_count = char_count_s.get(curr_char);
+        if(curr_char_count != null) char_count_s.put(curr_char, curr_char_count+1);
         else char_count_s.put(curr_char, 1);
 
         // Input string t
         curr_char = t.charAt(i);
         curr_char_count = char_count_t.get(curr_char);
-        if(curr_char_count != null && curr_char_count != 0) char_count_t.put(curr_char, curr_char_count+1);
+        if(curr_char_count != null) char_count_t.put(curr_char, curr_char_count+1);
         else char_count_t.put(curr_char, 1);
       }
       return char_count_s.equals(char_count_t);
